f973b220692314055d4abee5e452c6a3276da2c8
NakedSolidSnake <cristianosstec@gmail.com>
2019-03-09 15:24:56 -0300

    templates

---
+++ 00-Python-Alura-00-Template/01-Print_and_Variables.py
@@ -0,0 +1,19 @@
+#print ("Brazil won 5 world titles")
+
+#print ("Brazil", "won", 5, "world titles")
+
+#print ("Brazil", "won", 5, "world titles", sep="-")
+#print ("Brazil", "won", 5, "world titles", sep="\n")
+
+#variables
+country = "Brazil"
+titles = 5
+
+print(country, "won", titles, "world titles")
+
+
+dia = 15
+mes = 10
+ano = 2015
+
+print(dia, mes, ano, sep="/", end="\n")
